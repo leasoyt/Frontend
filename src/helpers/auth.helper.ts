7a1745a0e63964de4aa115b1a7bd025267a624ec
augusto <augustoaranda22@gmail.com>
2024-10-18 16:52:40 -0300

    registro e implementacion de logica y mas validaciones

--- src/helpers/auth.helper.ts
+++ src/helpers/auth.helper.ts
@@ -1,4 +1,23 @@
-import { IloginProps } from "@/interfaces/Interfaces.types";
+import { IloginProps, IRegisterProps } from "@/interfaces/Interfaces.types";
+
+export async function register(userData: IRegisterProps) {
+  try {
+    const res = await fetch(`${API_URL}`, {
+      method: "POST",
+      headers: {
+        "contente-type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    });
+    if (res.ok) {
+      return res.json();
+    } else {
+      alert("failed to register")
+    }
+  } catch (error:any) {
+    throw new Error (error);
+  }
+}
 
 export async function login(userData: IloginProps) {
   try {
@@ -12,7 +31,7 @@ export async function login(userData: IloginProps) {
     if (res.ok) {
       return res.json();
     } else {
-    alert ("failed to Log in")
+      alert("failed to Log in");
     }
   } catch (error: any) {
     throw new Error(error);
