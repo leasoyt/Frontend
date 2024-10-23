d7be8205eaa6adbb2292489e1b3ae4c6ba9868fb
juanlopcon07 <juan.21futmas07@gmail.com>
2024-10-23 00:29:57 -0500

    Vista administrador, cuenta, informacion de cuenta

---
+++ src/app/admin/cuenta/pagos/page.tsx
@@ -0,0 +1,10 @@
+import AdminCuentaPagosView from '@/views/AdminCuentaPagosView/AdminCuentaPagosView'
+import React from 'react'
+
+const page = () => {
+  return (
+    <AdminCuentaPagosView/>
+  )
+}
+
+export default page
