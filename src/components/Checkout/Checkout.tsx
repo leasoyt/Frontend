4d1665614d533475a25f8aa95f2ab4e3fed17bc7
garoto10 <ggejtman@gmail.com>
2024-10-29 16:15:44 -0300

    pasarela de pago a revisar

--- src/components/Checkout/Checkout.tsx
+++ src/components/Checkout/Checkout.tsx
@@ -1,19 +1,25 @@
-"use client";
-
+"use client"
 import React, { useState } from "react";
-import { useRouter } from 'next/router'; // Importa el hook useRouter
+import { useRouter } from 'next/router';
 
 const Checkout = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
-  const [success, setSuccess] = useState(false); // Nuevo estado para el éxito
+  const [success, setSuccess] = useState(false);
   const API_URL = process.env.NEXT_PUBLIC_API_URL;
-  const router = useRouter(); // Inicializa el router
+  const router = useRouter();
+
+  
+  const plan = {
+    name: "Plan Pro",
+    price: 19999, 
+    description: "Incluye acceso completo a todas las funcionalidades."
+  };
 
   const handlePayment = async () => {
     setLoading(true);
-    setError(null); // Resetea el error al iniciar el pago
-    setSuccess(false); // Resetea el estado de éxito
+    setError(null);
+    setSuccess(false);
 
     try {
       const response = await fetch(`${API_URL}/payments/create`, {
@@ -24,8 +30,8 @@ const Checkout = () => {
         body: JSON.stringify({
           items: [
             {
-              title: "Plan Pro",
-              unit_price: 19999,
+              title: plan.name,
+              unit_price: plan.price,
               quantity: 1,
             },
           ],
@@ -48,21 +54,14 @@ const Checkout = () => {
     }
   };
 
-  // Función para redirigir después del pago exitoso
-  const handleSuccess = () => {
-    setSuccess(true);
-    setTimeout(() => {
-      router.push('/registerRestaurant'); 
-    }, 3000); 
-  };
-
-  
-
   return (
     <div className="flex flex-col items-center justify-center">
       <h1 className="text-2xl font-bold mb-4">Pago con Mercado Pago</h1>
+      <p className="mb-4 text-center">
+        {plan.description} - Precio: ${plan.price / 100}
+      </p>
       {error && <p className="text-red-500 mb-4">{error}</p>}
-      {success && <p className="text-green-500 mb-4">¡Pago exitoso! Redirigiendo a registrar tu restaurante...</p>} {/* Mensaje de éxito */}
+      {success && <p className="text-green-500 mb-4">¡Pago exitoso! Redirigiendo a registrar tu restaurante...</p>}
       <button
         onClick={handlePayment}
         className={`bg-blue-500 text-white px-4 py-2 rounded-md ${
@@ -70,10 +69,10 @@ const Checkout = () => {
         }`}
         disabled={loading}
       >
-        {loading ? "Cargando..." : "Pagar"}
+        {loading ? "Cargando..." : `Pagar ${plan.name}`}
       </button>
     </div>
   );
-};
+}
 
 export default Checkout;
